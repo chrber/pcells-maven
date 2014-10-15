b45789024c3fb26f172e2b329283461a525d27c2
Christian Bernardt <christian.bernardt@desy.de>
2014-10-15 12:46:38 +0200

    Add Logable interface

---
+++ modules/org.pcells/src/main/java/org/pcells/util/Logable.java
@@ -0,0 +1,12 @@
+package org.pcells.util;
+
+/**
+ * Created by bernardt on 15/10/14 for pcells
+ */
+public interface Logable {
+    void log(java.lang.String s);
+
+    void elog(java.lang.String s);
+
+    void plog(java.lang.String s);
+}
