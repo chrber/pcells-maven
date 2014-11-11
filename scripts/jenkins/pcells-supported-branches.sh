9e72021477649cfd9390a0425701ff2ffe8f3dc4
Christian Bernardt <christian.bernardt@desy.de>
2014-11-11 17:12:20 +0100

    pcells: Remove zip file removal and second mavn package

--- scripts/jenkins/pcells-supported-branches.sh
+++ scripts/jenkins/pcells-supported-branches.sh
@@ -9,8 +9,6 @@ git clone https://github.com/dCache/pcells-maven.git;
 cd pcells-maven;
 git checkout dcache/${dCacheVersion};
 mvn package || echo 'First mvn package failed';
-zip -d  ~/.m2/repository/eu/emi/vomsjapi/2.0.6/vomsjapi-2.0.6.jar ../../../src/api/java/log4j.properties; || echo 'Removing problematic file from ~/.m2/repository/eu/emi/vomsjapi/2.0.6/vomsjapi-2.0.6.jar failed."
-mvn package;
 
 cd modules/org.pcells/target;
 tar czf pcells-$TAG.tar.gz ./*.jar
